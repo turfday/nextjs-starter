03b8c8caac677ff0f8c006ea320bf8b0465346b3
BarrySiegel <billing@turfday.com>
2025-06-04 09:25:35 -0700

    This is the first change this is the first chanbge.

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -4,7 +4,7 @@ export default function SmallCard({ Icon, title, slug }) {
   return (
     <a className="card-small" href={`/project/${slug}`}>
       <Icon w={153} h={163} />
-      <h3>{title}</h3>
+      <h3>{title}</h3>xxxxx
     </a>
   );
 }
